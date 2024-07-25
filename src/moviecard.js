46f1f82b2b6655dc0e3b6f893c72de2957e64466
Jeremiah Nyok <98920854+a1bx@users.noreply.github.com>
2024-07-25 14:26:41 +0300

    Update moviecard.js

--- src/moviecard.js
+++ src/moviecard.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-const movieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+const MovieCard = ({ movie }) => {
+  const { id, release_date, poster_path, title, media_type, vote_average } = movie;
+
+  const posterUrl = poster_path
+    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+    : "https://via.placeholder.com/400";
+
   return (
-    <div className="movie" key={imdbID}>
-      <div>
-        <p>{Year}</p>
-      </div>
-      {/* <div>
-        <p>{Rating}</p>
-      </div> */}
-      <div>
-        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+    <div className="movie" key={id}>
+      <div className="movie-info">
+        <p>{release_date ? release_date.substring(0, 4) : 'N/A'}</p>
       </div>
 
-      <div>
-        <span>{Type}</span>
-        <h3>{Title}</h3>
+      <div className="movie-poster">
+        <img src={posterUrl} alt={title} />
+      </div>
+
+      <div className="movie-details">
+        <span>{media_type || 'movie'}</span>
+        <h3>{title}</h3>
+      </div>
+
+      <div className="movie-rating">
+        <p>Rating: {vote_average ? vote_average : 'N/A'}</p>
       </div>
     </div>
   );
 }
 
-export default movieCard;
+export default MovieCard;
